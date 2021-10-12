592b6dfba638bba87e4cf95eecefea4bd4f6f229
isnabok <isnabok@gmail.com>
2021-10-12 22:23:52 +0300

    edit and add infomation

--- theory/practice/2/5_example.js
+++ theory/practice/2/5_example.js
@@ -11,8 +11,8 @@ fnNew(1); // 1 'Hello' 5
 
 // Значения по умолчанию для старого синтаксиса, где нет такой возможности
 function fnOld(a, b, c) {
-    b = b || b = 'Hello';
-    c = c || c =5;
+    b = b || 'Hello';
+    c = c || 5;
 }
 
 fnOld(1, 2, 3); // 1 2 3
@@ -30,3 +30,24 @@ function fnHash(args) {
 fnHash({a: [15, 32, 78], b: 'World', c: 78});
 fnHash({b: 'World'});
 fnHash({c: 78});
+
+'use strict';
+
+const f1 = (...args) => {
+    console.log(args);
+}
+
+f1(1, 5, 9);
+
+// Использование forEach для разбора неограниченного числа аргументов
+const f2 = (...args) => {
+    args.forEach (arg => {
+        console.log('Type: ' + typeof arg);
+        if(typeof arg === 'object') {
+            console.log('Value: ' + JSON.stringify(arg));
+        } else {
+            console.log('Value: ' + arg);
+        }
+    });
+}
+f2(5, 'world', [2, 'hello'], {field: 'value'});
