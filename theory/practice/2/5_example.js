f9230a2c5220c64516f6225a0710b0b61b319aa4
isnabok <isnabok@gmail.com>
2021-10-12 21:29:34 +0300

    edit function default

--- theory/practice/2/5_example.js
+++ theory/practice/2/5_example.js
@@ -8,3 +8,25 @@ function fnNew(a, b = 'Hello', c = 5) {
 fnNew(1, 2, 3); // 1 2 3
 fnNew(1, 2); // 1 2 5
 fnNew(1); // 1 'Hello' 5
+
+// Значения по умолчанию для старого синтаксиса, где нет такой возможности
+function fnOld(a, b, c) {
+    b = b || b = 'Hello';
+    c = c || c =5;
+}
+
+fnOld(1, 2, 3); // 1 2 3
+fnOld(1, 2); // 1 2 5
+fnOld(1); // 1 'Hello' 5
+
+// Hash style for default parameter values
+// Передача именнованных аргументоа
+function fnHash(args) {
+    args.a = args.a || [4, 45, 65];
+    args.b = args.b || 'Hello';
+    args.c = args.c || 100;
+}
+
+fnHash({a: [15, 32, 78], b: 'World', c: 78});
+fnHash({b: 'World'});
+fnHash({c: 78});
